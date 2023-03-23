ccbbdfcad4122b40b247b8c257d6dd28212d5d12
pzazzz <105593890+pzazzz@users.noreply.github.com>
2023-03-24 01:41:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,15 +12,39 @@ $.getJSON("https://ipapi.co/json/", function(data) {
     document.getElementById('icon').src = "http://openweathermap.org/img/w/" + weatherData.weather[0].icon + ".png";
   });
 
-  var londonUrl = "https://api.openweathermap.org/data/2.5/weather?q=London,UK&appid=bf8d15a80c89aa4f4c82ad6cbb3f5ac5";
-  
-  $.getJSON(londonUrl, function(londonData) {
+var londonUrl = "https://api.openweathermap.org/data/2.5/weather?q=London,UK&appid=bf8d15a80c89aa4f4c82ad6cbb3f5ac5";
+var newYorkUrl = "https://api.openweathermap.org/data/2.5/weather?q=New York,US&appid=bf8d15a80c89aa4f4c82ad6cbb3f5ac5";
+var tokyoUrl = "https://api.openweathermap.org/data/2.5/weather?q=Tokyo,JP&appid=bf8d15a80c89aa4f4c82ad6cbb3f5ac5";
+var dubaiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Dubai,AE&appid=bf8d15a80c89aa4f4c82ad6cbb3f5ac5";
+
+$.getJSON(londonUrl, function(londonData) {
     document.getElementById('weather-london').innerHTML = londonData.weather[0].description;
     document.getElementById('location-london').innerHTML = "London, UK";
     document.getElementById('temp-london').innerHTML = (londonData.main.temp - 273.15).toFixed(1);
     document.getElementById('icon-london').src = "http://openweathermap.org/img/w/" + londonData.weather[0].icon + ".png";
-  });
 });
+
+$.getJSON(newYorkUrl, function(newYorkData) {
+    document.getElementById('weather-newyork').innerHTML = newYorkData.weather[0].description;
+    document.getElementById('location-newyork').innerHTML = "New York, US";
+    document.getElementById('temp-newyork').innerHTML = (newYorkData.main.temp - 273.15).toFixed(1);
+    document.getElementById('icon-newyork').src = "http://openweathermap.org/img/w/" + newYorkData.weather[0].icon + ".png";
+});
+
+$.getJSON(tokyoUrl, function(tokyoData) {
+    document.getElementById('weather-tokyo').innerHTML = tokyoData.weather[0].description;
+    document.getElementById('location-tokyo').innerHTML = "Tokyo, JP";
+    document.getElementById('temp-tokyo').innerHTML = (tokyoData.main.temp - 273.15).toFixed(1);
+    document.getElementById('icon-tokyo').src = "http://openweathermap.org/img/w/" + tokyoData.weather[0].icon + ".png";
+});
+
+$.getJSON(dubaiUrl, function(dubaiData) {
+    document.getElementById('weather-dubai').innerHTML = dubaiData.weather[0].description;
+    document.getElementById('location-dubai').innerHTML = "Dubai, AE";
+    document.getElementById('temp-dubai').innerHTML = (dubaiData.main.temp - 273.15).toFixed(1);
+    document.getElementById('icon-dubai').src = "http://openweathermap.org/img/w/" + dubaiData.weather[0].icon + ".png";
+});
+
 
 
   var request = new XMLHttpRequest();
